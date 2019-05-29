ca9ce7bc9e7905627866c5248f2d2b82ad75712d
Duncan McClean <duncan@mcclean.co.uk>
2019-05-29 11:21:49 +0100

    Refactored cookie notice into a view

--- CookieNotice/CookieNoticeTags.php
+++ CookieNotice/CookieNoticeTags.php
@@ -14,75 +14,9 @@ class CookieNoticeTags extends Tags
 
     public function index()
     {
-        $html = "<!-- Start of Cookie Notice addon -->
-                <div id=\"notice\" class=\"cookie-notice\">
-                    <div class=\"cookie-notice-container\">
-                        <p class=\"cookie-notice-text\">" . $this->text . "</p>
-                        <div class=\"cookie-notice-buttons\">
-                            <button class=\"cookie-notice-button\" id=\"accept\">Accept</button>
-                        </div>
-                    </div>
-                </div>";
-
-        $css = "<style>
-                    .cookie-notice {
-                        background-color: #ffffff;
-                        bottom: 0px;
-                        left: 0px;
-                        right: 0px;
-                        position: fixed;
-                        padding: 4px;
-                        font-family: sans-serif;
-                    }
-                    .cookie-notice-container {
-                        width: 60%;
-                        margin: auto;
-                        display: flex;
-                        flex-direction: row;
-                        justify-content: space-between;
-                        align-items: center;
-                    }
-                    .cookie-notice-button {
-                        background-color: #3490DC;
-                        text-align: center;
-                        color: white;
-                        padding: 5px;
-                        margin: 2px;
-                        border-radius: 5px;
-                    }
-                    .cookie-notice-button:hover {
-                        background-color: #6CB2EB;
-                    }
-                </style>";
-        
-        $js = "<script>
-                function acceptCookie() {
-                    var date = new Date();
-                    date.setTime(date.getTime() + (90*24*60*60*1000));
-                    var expires = \"expires=\"+date.toUTCString();
-
-                    document.cookie = \"cookieNotice\" + \"=\" + \"accepted\" + \"; \" + expires;
-                    checkCookie();
-                }
-
-                function checkCookie() {
-                    if (document.cookie.indexOf('cookieNotice') != -1) {
-                        document.getElementById('notice').innerHTML = '';
-                    }
-                }
-
-                document.addEventListener(\"DOMContentLoaded\", function(event) { 
-                    checkCookie()
-                });
-
-                document.getElementById('accept').onclick = acceptCookie;
-            </script>
-            <!-- End of Cookie Notice addon -->";
-
-        if($this->styles === true) {
-            return $html . $js;
-        } else {
-            return $html . $css . $js;
-        }
+        return $this->view('notice', [
+            'text' => $this->text,
+            'styles' => $this->styles
+        ]);
     }
 }
---
+++ CookieNotice/resources/views/notice.blade.php
@@ -0,0 +1,69 @@
+<!-- Start of Cookie Notice addon -->
+<div id="notice" class="cookie-notice">
+    <div class="cookie-notice-container">
+        <p class="cookie-notice-text">{{ $text }}</p>
+        <div class="cookie-notice-buttons">
+            <button class="cookie-notice-button" id="accept">Accept</button>
+        </div>
+    </div>
+</div>
+
+@if($styles != true)
+<style>
+    .cookie-notice {
+        background-color: #ffffff;
+        bottom: 0px;
+        left: 0px;
+        right: 0px;
+        position: fixed;
+        padding: 4px;
+        font-family: sans-serif;
+    }
+
+    .cookie-notice-container {
+        width: 60%;
+        margin: auto;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .cookie-notice-button {
+        background-color: #3490DC;
+        text-align: center;
+        color: white;
+        padding: 5px;
+        margin: 2px;
+        border-radius: 5px;
+    }
+
+    .cookie-notice-button:hover {
+        background-color: #6CB2EB;
+    }
+</style>
+@endif
+
+<script>
+    function acceptCookie() {
+        var date = new Date();
+        date.setTime(date.getTime() + (90*24*60*60*1000));
+        var expires = "expires="+date.toUTCString();
+
+        document.cookie = "cookieNotice" + "=" + "accepted" + "; " + expires;
+        checkCookie();
+    }
+
+    function checkCookie() {
+        if (document.cookie.indexOf('cookieNotice') != -1) {
+            document.getElementById('notice').innerHTML = '';
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event) { 
+        checkCookie()
+    });
+
+    document.getElementById('accept').onclick = acceptCookie;
+</script>
+<!-- End of Cookie Notice addon -->
