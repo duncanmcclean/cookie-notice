7bd49c2b3efe275239b0e36763f6bb0e9854171f
gabrieltakacs <gtakacs@gtakacs.sk>
2020-04-12 09:36:42 +0200

    Make consent button label being settable in control panel

--- CookieNotice/CookieNoticeTags.php
+++ CookieNotice/CookieNoticeTags.php
@@ -12,8 +12,9 @@ class CookieNoticeTags extends Tags
             'domainName' => request()->getHost(),
             'cookieName' => $this->getConfig('cookie-name', 'cookie_notice'),
             'noticeText' => $this->getConfig('text', 'Your experience on this site will be improved by allowing cookies.'),
+            'buttonLabel' => $this->getConfig('button_label', 'Allow cookies'),
             'noticeStyles' => $this->getConfig('disable_styles', false),
-            'noticeLocation' => $this->getConfig('location', 'bottom')
+            'noticeLocation' => $this->getConfig('location', 'bottom'),
         ]);
     }
 }
