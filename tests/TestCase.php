e7606f78adb03f5d8134590440d020d17bdb70e0
Duncan McClean <damcclean@users.noreply.github.com>
2020-04-28 10:37:10 +0100

    Exception handler no longer required

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,13 +10,6 @@ use Statamic\Statamic;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    protected function setUp(): void
-    {
-        require_once(__DIR__.'/ExceptionHandler.php');
-
-        parent::setUp();
-    }
-
     protected function getPackageProviders($app)
     {
         return [
