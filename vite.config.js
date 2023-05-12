8cadb5ab29e3eee1ae35524c2879d58582bcd4a8
Florian Brinkmann <florian.brinkmann@visuellverstehen.de>
2023-05-12 07:57:34 +0200

    fix: wrong CSS bundle file name after switching to Vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name][extname]',
+      }
+    }
+  },
   plugins: [
     laravel({
       hotFile: "vite.hot",
       publicDirectory: "dist",
+      buildDirectory: "css",
       input: ["resources/css/cookie-notice.css"],
     }),
   ],
