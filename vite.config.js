b93b754a4b397057fe8a06a9564f29c4763f2715
Florian Brinkmann <florian.brinkmann@visuellverstehen.de>
2023-05-12 08:03:59 +0200

    chore: don’t generate a manifest.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
   build: {
+    manifest: false,
     rollupOptions: {
       output: {
         assetFileNames: '[name][extname]',
