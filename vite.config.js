b88bc6c92e43e9480d1313dab4491f529c2dd790
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-05-16 17:30:29 +0100

    Fix Vite once and for all (#72)
    * Fix Vite once and for all
    
    * Apply Code Style changes
    
    ---------
    
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-  build: {
-    manifest: false,
-    rollupOptions: {
-      output: {
-        assetFileNames: '[name][extname]',
-      }
-    }
-  },
   plugins: [
     laravel({
       hotFile: "vite.hot",
       publicDirectory: "dist",
-      buildDirectory: "css",
       input: ["resources/css/cookie-notice.css"],
     }),
   ],
