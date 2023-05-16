618b64de0bc4a5b8cd5e35ef83b2d4d04c08109f
Duncan McClean <duncan@duncanmcclean.com>
2023-05-16 19:29:25 +0100

    Override Statamic's `bootPublishAfterInstall` method so CSS is automatically published

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -34,10 +34,6 @@ class ServiceProvider extends AddonServiceProvider
 
     protected function bootVendorAssets()
     {
-        // $this->publishes([
-        //     __DIR__.'/../dist' => public_path('vendor/cookie-notice'),
-        // ], 'cookie-notice');
-
         $this->publishes([
             __DIR__.'/../config/cookie-notice.php' => config_path('cookie-notice.php'),
         ], 'cookie-notice-config');
@@ -48,6 +44,27 @@ class ServiceProvider extends AddonServiceProvider
 
         $this->mergeConfigFrom(__DIR__.'/../config/cookie-notice.php', 'cookie-notice');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'cookie-notice');
+
+        return $this;
+    }
+
+    protected function bootPublishAfterInstall()
+    {
+        if (! $this->publishAfterInstall) {
+            return $this;
+        }
+
+        // Temp: Adds $this->publishables to the list of publishable assets.
+        if (empty($this->scripts) && empty($this->stylesheets) && empty($this->vite) && empty($this->publishables)) {
+            return $this;
+        }
+
+        Statamic::afterInstalled(function ($command) {
+            $command->call('vendor:publish', [
+                '--tag' => $this->getAddon()->slug(),
+                '--force' => true,
+            ]);
+        });
 
         return $this;
     }
