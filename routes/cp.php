d4cc107783e38e2ccb1e1ee4b0c227bc4ddedc83
Duncan McClean <duncan@duncanmcclean.com>
2025-05-31 14:24:26 +0100

    Take advantage of new `PublishForm` abstraction

--- routes/cp.php
+++ routes/cp.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('cookie-notice')->name('cookie-notice.')->group(function () {
     Route::get('scripts', [ScriptsController::class, 'edit'])->name('scripts.edit');
-    Route::post('scripts', [ScriptsController::class, 'update'])->name('scripts.update');
+    Route::patch('scripts', [ScriptsController::class, 'update'])->name('scripts.update');
 });
