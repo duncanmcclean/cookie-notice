15621ee1c80ba0d7a7424f35fea68e11053ca593
Duncan McClean <duncan@duncanmcclean.com>
2025-07-03 13:12:58 +0100

    Remove visibleValues fix

--- src/Http/Controllers/CP/ScriptsController.php
+++ src/Http/Controllers/CP/ScriptsController.php
@@ -27,34 +27,9 @@ class ScriptsController extends CpController
     {
         abort_if(Auth::user()->cant('manage scripts'), 403);
 
-        $values = PublishForm::make(Blueprint::blueprint())->submit($request->values);
+        $values = PublishForm::make(Blueprint::blueprint())->submit($request->all());
 
-        $values = collect($values)->map(function ($scripts, $group) {
-            // We only want to manipulate the consent group fields.
-            if (! collect(config('cookie-notice.consent_groups'))->keyBy('handle')->has($group)) {
-                return $scripts;
-            }
-
-            return collect($scripts)->map(function (array $script) {
-                // When you deselect all consent types, we need to save an empty array to
-                // prevent it falling back to the field's default value (all consent types).
-                if ($script['script_type'] === 'google-tag-manager') {
-                    if (! isset($script['gtm_consent_types'])) {
-                        $script['gtm_consent_types'] = [];
-                    }
-
-                    return $script;
-                }
-
-                // Currently, Statamic's PublishForm component submits *all* values, rather than just the *visible* values.
-                // This should hopefully be fixed in Statamic 6, but until then, we need to filter out the gtm_consent_types key manually.
-                unset($script['gtm_consent_types']);
-
-                return $script;
-            })->all();
-        });
-
-        Scripts::save($values->all());
+        Scripts::save($values);
 
         return response()->json(['message' => 'Scripts saved']);
     }
